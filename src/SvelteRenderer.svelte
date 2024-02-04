fa19dccaac47bcfaf335aca1183bca1789be1af3
Robin Danzinger <robin.danzinger@gmx.com>
2024-02-04 16:14:09 +0100

    add inital implementation

---
+++ src/SvelteRenderer.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { onMount } from 'svelte'
+  export let component
+  export let selectedExample
+  export let das
+  export let afterRenderHook = async () => {}
+
+  let redirectData = {}
+  $: {
+    if (selectedExample) {
+      redirectData = {}
+    }
+  }
+
+  onMount(async () => {
+    await afterRenderHook()
+  })
+
+  function handleComponentOut(evt) {
+    if (das.out) {
+      das.out.forEach((out) => {
+        if (evt.detail[out.name]) {
+          if (selectedExample.redirect && selectedExample.redirect[out.name]) {
+            const input = selectedExample.redirect[out.name]
+            redirectData[input] = evt.detail[out.name]
+          }
+        }
+      })
+    }
+  }
+</script>
+
+<svelte:component
+  this={component}
+  {...selectedExample?.input}
+  {...redirectData}
+  on:out={handleComponentOut}
+/>
